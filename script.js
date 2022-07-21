f14e7f2ce89e7ec457dde17ce7733f627303610e
Dhuruv Singh <84073153+DhuruvSingh@users.noreply.github.com>
2022-07-22 02:11:13 +0530

    fixed all erros

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const menu = document.querySelector('.menu');
 const hamburger = document.querySelector('.hamburger');
-const closeIcon = document.querySelector('.closeicon');
-const menuIcon = document.querySelector('.menuicon');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
 
 function toggleMenu() {
-  if (menu.classList.contains('showmenu')) {
-    menu.classList.remove('showmenu');
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
     closeIcon.style.display = 'none';
     menuIcon.style.display = 'block';
   } else {
-    menu.classList.add('showmenu');
+    menu.classList.add('showMenu');
     closeIcon.style.display = 'block';
     menuIcon.style.display = 'none';
   }
@@ -18,7 +18,7 @@ function toggleMenu() {
 menu.addEventListener('click', toggleMenu);
 hamburger.addEventListener('click', toggleMenu);
 
-var projects = [
+const projects = [
   {
     title: 'Keeping track of hundreds of components',
     description:
@@ -259,7 +259,6 @@ document.getElementById('work-container').innerHTML = `
 </div>
 </div>
 `;
-
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
@@ -300,67 +299,61 @@ function closeModal(modal) {
 openModalButtons.forEach((button) => {
   button.addEventListener('click', (event) => {
     if (event.currentTarget.classList[0] === 'project-1') {
-        (document.getElementById('project-image-desktop').src = projects[0].image),
-        (document.getElementById('modal-title').innerHTML = projects[0].title),
-        (document.getElementById('modal-btns').innerHTML = `<button action="${projects[0].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
+      (document.getElementById('project-image-desktop').src = projects[0].image)(document.getElementById('modal-title').innerHTML = projects[0].title),
+      (document.getElementById('modal-btns').innerHTML = `<button action="${projects[0].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
                     <button action="${projects[0].Source}" type="submit">See Source <i class="fa-brands fa-github"></i></button>`),
-        (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[0].technologies[0]}</p></li>
+      (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[0].technologies[0]}</p></li>
                     <li><p>${projects[0].technologies[1]}</p></li>
                     <li><p>${projects[0].technologies[2]}</p></li>
                     <li><p>${projects[0].technologies[3]}</p></li>
                     <li><p>${projects[0].technologies[4]}</p></li>
                     <li><p>${projects[0].technologies[5]}</p></li>`),
-        (document.getElementById('modal-description').innerHTML = projects[0].description)
-
-      } else if (event.currentTarget.classList[0] === 'project-2') {
-        (document.getElementById('project-image-desktop').src = projects[1].image),
-        (document.getElementById('modal-title').innerHTML = projects[1].title),
-        (document.getElementById('modal-btns').innerHTML = `<button action="${projects[1].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
+      (document.getElementById('modal-description').innerHTML = projects[0].description);
+    } else if (event.currentTarget.classList[0] === 'project-2') {
+      (document.getElementById('project-image-desktop').src = projects[1].image),
+      (document.getElementById('modal-title').innerHTML = projects[1].title),
+      (document.getElementById('modal-btns').innerHTML = `<button action="${projects[1].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
                       <button action="${projects[1].Source}" type="submit">See Source <i class="fa-brands fa-github"></i></button>`),
-        (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[1].technologies[0]}</p></li>
+      (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[1].technologies[0]}</p></li>
                       <li><p>${projects[1].technologies[1]}</p></li>
                       <li><p>${projects[1].technologies[2]}</p></li>`),
-        (document.getElementById('modal-description').innerHTML = projects[1].description)
-
-      } else if (event.currentTarget.classList[0] === 'project-3') {
-        (document.getElementById('project-image-desktop').src = projects[2].image),
-        (document.getElementById('modal-title').innerHTML = projects[2].title),
-        (document.getElementById('modal-btns').innerHTML = `<button action="${projects[2].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
+      (document.getElementById('modal-description').innerHTML = projects[1].description);
+    } else if (event.currentTarget.classList[0] === 'project-3') {
+      (document.getElementById('project-image-desktop').src = projects[2].image),
+      (document.getElementById('modal-title').innerHTML = projects[2].title),
+      (document.getElementById('modal-btns').innerHTML = `<button action="${projects[2].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
                         <button action="${projects[2].Source}" type="submit">See Source <i class="fa-brands fa-github"></i></button>`),
-        (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[2].technologies[0]}</p></li>
+      (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[2].technologies[0]}</p></li>
                         <li><p>${projects[2].technologies[1]}</p></li>
                         <li><p>${projects[2].technologies[2]}</p></li>`),
-        (document.getElementById('modal-description').innerHTML = projects[2].description)
-
-      } else if (event.currentTarget.classList[0] === 'project-4') {
-        (document.getElementById('project-image-desktop').src = projects[3].image),
-        (document.getElementById('modal-title').innerHTML = projects[3].title),
-        (document.getElementById('modal-btns').innerHTML = `<button action="${projects[3].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
+      (document.getElementById('modal-description').innerHTML = projects[2].description);
+    } else if (event.currentTarget.classList[0] === 'project-4') {
+      (document.getElementById('project-image-desktop').src = projects[3].image),
+      (document.getElementById('modal-title').innerHTML = projects[3].title),
+      (document.getElementById('modal-btns').innerHTML = `<button action="${projects[3].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
                           <button action="${projects[3].Source}" type="submit">See Source <i class="fa-brands fa-github"></i></button>`),
-        (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[3].technologies[0]}</p></li>
+      (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[3].technologies[0]}</p></li>
                           <li><p>${projects[3].technologies[1]}</p></li>
                           <li><p>${projects[3].technologies[2]}</p></li>`),
-        (document.getElementById('modal-description').innerHTML = projects[3].description)
-
-      } else if (event.currentTarget.classList[0] === 'project-5') {
-        (document.getElementById('project-image-desktop').src = projects[4].image),
-        (document.getElementById('modal-title').innerHTML = projects[4].title),
-        (document.getElementById('modal-btns').innerHTML = `<button action="${projects[4].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
+      (document.getElementById('modal-description').innerHTML = projects[3].description);
+    } else if (event.currentTarget.classList[0] === 'project-5') {
+      (document.getElementById('project-image-desktop').src = projects[4].image),
+      (document.getElementById('modal-title').innerHTML = projects[4].title),
+      (document.getElementById('modal-btns').innerHTML = `<button action="${projects[4].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
                             <button action="${projects[4].Source}" type="submit">See Source <i class="fa-brands fa-github"></i></button>`),
-        (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[4].technologies[0]}</p></li>
+      (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[4].technologies[0]}</p></li>
                             <li><p>${projects[4].technologies[1]}</p></li>
                             <li><p>${projects[4].technologies[2]}</p></li>`),
-        (document.getElementById('modal-description').innerHTML = projects[4].description)
-
-      } else if (event.currentTarget.classList[0] === 'project-6') {
-        (document.getElementById('project-image-desktop').src = projects[5].image),
-        (document.getElementById('modal-title').innerHTML = projects[5].title),
-        (document.getElementById('modal-btns').innerHTML = `<button action="${projects[5].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
+      (document.getElementById('modal-description').innerHTML = projects[4].description);
+    } else if (event.currentTarget.classList[0] === 'project-6') {
+      (document.getElementById('project-image-desktop').src = projects[5].image),
+      (document.getElementById('modal-title').innerHTML = projects[5].title),
+      (document.getElementById('modal-btns').innerHTML = `<button action="${projects[5].See_Live}" type="submit">See Live <i class="fa-solid fa-up-right-from-square"></i></button>
                               <button action="${projects[5].Source}" type="submit">See Source <i class="fa-brands fa-github"></i></button>`),
-        (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[5].technologies[0]}</p></li>
+      (document.getElementById('modal-tag').innerHTML = `<li><p>${projects[5].technologies[0]}</p></li>
                               <li><p>${projects[5].technologies[1]}</p></li>
                               <li><p>${projects[5].technologies[2]}</p></li>`),
-        (document.getElementById('modal-description').innerHTML = projects[5].description)
-      }
+      (document.getElementById('modal-description').innerHTML = projects[5].description);
+    }
   });
 });
